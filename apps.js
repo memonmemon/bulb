f60141c524b8ccfed0edceeaa5dcb15c6efa327f
memonmemon <anusattari94@gmail.com>
2024-05-16 16:25:37 +0500

    bulb

---
+++ apps.js
@@ -0,0 +1,7 @@
+function bulboff(){
+    var bulbobj = document.querySelector(".bulboff");
+    bulbobj.setAttribute("src", "images.jpg");
+}function bulbon(){
+    var bulbobj = document.querySelector(".bulboff");
+    bulbobj.setAttribute("src", "image.jpg");
+}
